f2291d00b6c0721a6186230a3296c1c3a46dc129
Ryan Karling <85957240+karling17@users.noreply.github.com>
2023-07-28 08:37:48 -0300

    Inicio

---
+++ login_falhou.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Falhou</title>
+    <script>
+        alert("Falha na autenticação. Verifique usuário e senha.");
+        history.back();
+    </script>
+</head>
+<body>
+</body>
+</html>
---
+++ login_sucesso.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Efetuado com Sucesso</title>
+</head>
+<body>
+    <h1>Login Efetuado com Sucesso</h1>
+    <!-- Coloque aqui o conteúdo que deseja exibir após o login bem-sucedido -->
+</body>
+</html>
