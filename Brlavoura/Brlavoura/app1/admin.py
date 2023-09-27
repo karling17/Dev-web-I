3b039c0a66ebb8be61ba643544b27c346ccceefa
Ryan Karling <85957240+karling17@users.noreply.github.com>
2023-09-27 09:16:02 -0300

    Criado classes e realizado ajustes no banco

--- Brlavoura/Brlavoura/app1/admin.py
+++ Brlavoura/Brlavoura/app1/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from app1.models import Usuario
+from app1.models import Users
+from app1.models import Farm
 
 # Register your models here.
-admin.site.register(Usuario)
+admin.site.register(Users)
+admin.site.register(Farm)
--- Brlavoura/Brlavoura/app1/models.py
+++ Brlavoura/Brlavoura/app1/models.py
@@ -1,13 +1,67 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    PROFILES = (
+    ('farmer', 'Fazendeiro'),
+    ('agronomist', 'Agrônomo'),
+    ('cooperative', 'Cooperativa'),
+    )
+
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    profile = models.CharField(max_length=20, choices=PROFILES)
+
+    def __str__(self):
+        return self.profile
+
 class Farm(models.Model): 
-    id = models.AutoField(primary_key=True, null=False)
     name = models.CharField(max_length=100)
-    farmerId = models.ForeignKey(null=True)
-    AgronomistId = models.ForeignKey(null=True)
-    cooperativeID = models.ForeignKey(null=True)
+    farmerId = models.ForeignKey(Users,
+                                  null=False,
+                                  on_delete=models.CASCADE, 
+                                  related_name='farms',
+                                  limit_choices_to={'profile':'farmer'}
+                                )
+    AgronomistId = models.ForeignKey(Users,
+                                  null=False,
+                                  on_delete=models.CASCADE,
+                                  related_name='Agronomists', 
+                                  limit_choices_to={'profile':'agronomist'}
+                                )
+    cooperativeID = models.ForeignKey(Users,
+                                  null=False,
+                                  on_delete=models.CASCADE,
+                                  related_name='cooperatives', 
+                                  limit_choices_to={'profile':'cooperative'}
+                                )
     geolocalization = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.name
+    
+class Harvest(models.Model):
+    localization = models.CharField(max_length=100)
+    farmId = models.ForeignKey(Farm,
+                               null=False, 
+                               on_delete=models.CASCADE,
+                               related_name='harvests',
+                               )
+    
+class Tillage(models.Model):
+    startdate = 
+    active = models.BooleanField(default=False)
+    enddate =
+    health = models.CharField(max_length=100)
+    harvestId = models.ForeignKey(Harvest,
+                                  null=False,
+                                  on_delete=models.CASCADE,
+                                  related_name='tillages')
+
+
+
+
+    
+    
+
