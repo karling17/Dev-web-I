e99866170d4147e10ae5d4b9febb2aaa9566e53b
Ryan Karling <85957240+karling17@users.noreply.github.com>
2023-09-28 19:33:49 -0300

    Criacao e ajuste de classes

--- Brlavoura/Brlavoura/app1/admin.py
+++ Brlavoura/Brlavoura/app1/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from app1.models import Users
 from app1.models import Farm
+from app1.models import Harvest
+from app1.models import Tillage
+from app1.models import Pictures
+from app1.models import Humidity
+from app1.models import References
 
 # Register your models here.
 admin.site.register(Users)
 admin.site.register(Farm)
+admin.site.register(Harvest)
+admin.site.register(Tillage)
+admin.site.register(Pictures)
+admin.site.register(Humidity)
+admin.site.register(References)
--- Brlavoura/Brlavoura/app1/models.py
+++ Brlavoura/Brlavoura/app1/models.py
@@ -50,14 +50,55 @@ class Harvest(models.Model):
                                )
     
 class Tillage(models.Model):
-    startdate = 
+    SEEDS = (
+    ('Thompson_Seedless', 'Uva Thompson Seedless'),
+    ('Red_Globe', 'Uva Red Globe'),
+    ('Flame_Seedless', 'Uva Flame Seedless'),
+    ('Concord', 'Uva Concord'),
+    ('Muscat', 'Uva Muscat'),
+    ('Cabernet_Sauvignon', 'Uva Cabernet Sauvignon'),
+    ('Chardonnay', 'Uva Chardonnay'),
+    ('Merlot', 'Uva Merlot'),
+    ('Pinot_Noir', 'Uva Pinot Noir'),
+    ('Zinfandel', 'Uva Zinfandel'),
+    )
+
+    startdate = models.DateField(auto_now=True)
     active = models.BooleanField(default=False)
-    enddate =
+    enddate = models.DateField()
     health = models.CharField(max_length=100)
+    usedseed = models.CharField(max_length=60, choices=SEEDS)
     harvestId = models.ForeignKey(Harvest,
                                   null=False,
                                   on_delete=models.CASCADE,
                                   related_name='tillages')
+    
+class Pictures(models.Model): 
+    timestamp = models.DateTimeField(auto_now=True)
+    notes = models.CharField(max_length=400)
+    file = models.CharField(max_length=100)
+    tillagedId = models.ForeignKey(Tillage,
+                                   null=False,
+                                  on_delete=models.CASCADE,
+                                  related_name='picsoftillage'
+                                   )
+
+class Humidity(models.Model):
+    timestamp = models.DateTimeField(auto_now=True)
+    humidity = models.FloatField(max_length=4)
+    tillageId = models.ForeignKey(Tillage,
+                                  null=False,
+                                  on_delete=models.CASCADE,
+                                  related_name='tillages'
+                                  )
+    
+class References(models.Model):
+    timestamp = models.DateTimeField(auto_now=True)
+    internalRef = models.FloatField(max_length=4)
+    externalRef = models.FloatField(max_length=4)
+
+
+
 
 
 
